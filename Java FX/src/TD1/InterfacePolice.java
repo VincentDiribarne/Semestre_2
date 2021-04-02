5d773f8db33067146243715aaa3c6145ba6e231c
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-04-02 19:08:05 +0200

    Ajout de JavaFX

---
+++ Java FX/src/TD1/InterfacePolice.java
@@ -0,0 +1,24 @@
+package TD1;
+
+public class InterfacePolice {
+
+    public void AnchorPane() {
+
+    }
+
+    public void FlowPane() {
+
+    }
+
+    public void TitlePane() {
+
+    }
+
+    public void FlowPane2() {
+
+    }
+
+    public void GridPane() {
+
+    }
+}
